9127a949bc7e2855d88139a261e7dfcde6ef756a
reinole <ole.christian.reinhartsen@trumf.no>
2024-03-02 08:43:56 +0100

    purchase 10 lollipops added

--- src/app/clientComponents/shopKeeper.js
+++ src/app/clientComponents/shopKeeper.js
@@ -4,28 +4,39 @@ import { GameData } from '../page'
 
 export const ShopKeeper = ({ candies, setCandies }) => {
     const [state, dispatch] = useContext(GameData)
+    console.log(candies)
 
     useEffect(() => {
-        if (candies === 60) {
+        if (candies > 59 && !state.shopKeeperSeen) {
             dispatch({ type: reducerTypes.TOGGLE_SHOPKEEPER })
         }
-    }, [candies, dispatch])
+        if (candies > 499 && !state.tenLollipopsVisible) {
+            dispatch({ type: reducerTypes.TOGGLE_TEN_LOLLIPOPS })
+        }
+    }, [candies, dispatch, state.shopKeeperSeen, state.tenLollipopsVisible])
+
+    const buyLollipop = (candies, lollipops) => {
+        if (lollipops === 10) {
+            dispatch({ type: reducerTypes.BUY_TEN_LOLLIPOPS })
+            setCandies(candies - 500)
+        }
+
+        if (lollipops === 1) {
+            dispatch({ type: reducerTypes.BUY_SINGLE_LOLLIPOP })
+            setCandies(candies - 60)
+        }
+    }
 
     if (state.shopKeeperSeen === false) {
         return null
-    }
-
-    const buyLollipop = (candies) => {
-        dispatch({ type: reducerTypes.BUY_SINGLE_LOLLIPOP })
-        setCandies(candies - 60)
     }
 
     return (
         <div>
             <h1>The Candy merchant</h1>
             <div>
-                <button onClick={() => buyLollipop(candies)}>Buy 1 lollipop (60 candies)</button>
-
+                <button disabled={candies < 60} onClick={() => buyLollipop(candies, 1)}>Buy 1 lollipop (60 candies)</button>
+                {state.tenLollipopsVisible && <button disabled={candies < 500} onClick={() => buyLollipop(candies, 10)}>Buy 10 lollipop (500 candies)</button>}
             </div>
         </div>
     );
--- src/app/reducer/candiesReducer.js
+++ src/app/reducer/candiesReducer.js
@@ -2,7 +2,8 @@ export const initialState = {
     candiesEaten: 0,
     candiesThrown: 0,
     lollipops: 0,
-    shopKeeperSeen: false
+    shopKeeperSeen: false,
+    tenLollipopsVisible: false
 }
 
 export const reducerTypes = {
@@ -10,10 +11,13 @@ export const reducerTypes = {
     THROW: 'THROW',
     RESET: 'RESET',
     TOGGLE_SHOPKEEPER: "TOGGLE_SHOPKEEPER",
-    BUY_SINGLE_LOLLIPOP: "BUY_SINGLE_LOLLIPOP"
+    TOGGLE_TEN_LOLLIPOPS: "TOGGLE_TEN_LOLLIPOPS",
+    BUY_SINGLE_LOLLIPOP: "BUY_SINGLE_LOLLIPOP",
+    BUY_TEN_LOLLIPOPS: "BUY_TEN_LOLLIPOPS",
 }
 
 export const reducer = (state, action) => {
+    console.log(state, action)
     switch (action.type) {
         case 'EAT':
             return { ...state, candiesEaten: state.candiesEaten + action.payload }
@@ -21,9 +25,12 @@ export const reducer = (state, action) => {
             return { ...state, candiesThrown: state.candiesThrown + action.payload }
         case 'BUY_SINGLE_LOLLIPOP':
             return { ...state, lollipops: state.lollipops + 1 }
-
+        case 'BUY_TEN_LOLLIPOPS':
+            return { ...state, lollipops: state.lollipops + 10 }
         case 'TOGGLE_SHOPKEEPER':
             return { ...state, shopKeeperSeen: true }
+        case 'TOGGLE_TEN_LOLLIPOPS':
+            return { ...state, tenLollipopsVisible: true }
         case 'RESET':
             return initialState
         // case 'SAVE':
